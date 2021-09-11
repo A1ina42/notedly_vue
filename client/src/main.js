d839e171523e66ea0ffba2dd37a3c879e47479f6
A1ina42 <87566639+A1ina42@users.noreply.github.com>
2021-09-11 20:48:04 +0300

    add apollo-vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,49 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import VueApollo from "vue-apollo";
+import ApolloClient from "apollo-client";
+import {InMemoryCache} from "apollo-cache-inmemory";
+import {setContext} from "apollo-link-context";
+import {createHttpLink} from "apollo-link-http";
+import dotenv from "dotenv";
+
+const dotEnvOptions = {
+	path: "/.env",
+};
+
+dotenv.config(dotEnvOptions);
+
+Vue.use(VueApollo);
+
+const uri = process.env.VUE_APP_API_URI;
+const httpLink = createHttpLink({uri});
+const cache = new InMemoryCache();
+
+const authLink = setContext((_, {headers}) => {
+	return {
+		headers: {
+			...headers,
+			authorization: localStorage.getItem("token") || "",
+		},
+	};
+});
+
+const apolloClient = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache,
+	resolvers: {},
+});
+
+const apolloProvider = new VueApollo({
+	defaultClient: apolloClient,
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	render: (h) => h(App),
+	apolloProvider,
+}).$mount("#app");
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -9,6 +9,11 @@
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
 import Button from "../components/Button";
+import {useQuery, gql} from "apollo-client";
+import {GET_NOTES} from "../gql/query";
+const {data, loading, error, fetchMore} = useQuery(GET_NOTES);
+console.log(data);
+
 export default {
 	name: "Home",
 	components: {
