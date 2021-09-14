11e01bfbe340fe24a1e50a4ba422a91e1bc880ac
A1ina42 <87566639+A1ina42@users.noreply.github.com>
2021-09-14 19:28:35 +0300

    add pagination

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,6 +4,7 @@
 		<p v-if="error">Error!</p>
 		<div v-if="noteFeed">
 			<NoteFeed :notes="noteFeed.notes"></NoteFeed>
+			<Button v-if="noteFeed.hasNextPage" @click="loadMore()">Load more</Button>
 		</div>
 	</div>
 </template>
@@ -11,24 +12,54 @@
 <script>
 import {GET_NOTES} from "../gql/query";
 import NoteFeed from "../components/NoteFeed";
+import Button from "../components/Button";
 
 export default {
 	name: "Home",
 	components: {
 		NoteFeed,
+		Button,
 	},
 	data() {
 		return {
 			error: null,
+			cursor: null,
 		};
 	},
 	apollo: {
 		noteFeed: {
 			query: GET_NOTES,
+			variables() {
+				return {
+					cursor: this.cursor,
+				};
+			},
 			error(error) {
 				this.error = JSON.stringify(error.message);
 				console.error(error);
 			},
+		},
+	},
+	methods: {
+		loadMore() {
+			this.$apollo.queries.noteFeed.fetchMore({
+				variables: {
+					cursor: this.noteFeed.cursor,
+				},
+				updateQuery: (previousResult, {fetchMoreResult}) => {
+					return {
+						noteFeed: {
+							cursor: fetchMoreResult.noteFeed.cursor,
+							hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+							notes: [
+								...previousResult.noteFeed.notes,
+								...fetchMoreResult.noteFeed.notes,
+							],
+							__typename: "noteFeed",
+						},
+					};
+				},
+			});
 		},
 	},
 };
